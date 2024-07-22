f5989cd2e3a03d3f987ddc6d0520b92815e3222e
OlgaBieliaieva <belyaevaolya0@gmail.com>
2024-07-22 12:59:41 +0300

    finished test page

--- js/test-form.js
+++ js/test-form.js
@@ -1,15 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const formItems = document.querySelectorAll(".test-form fieldset");
-  const progressBar = document.querySelector(".progress-bar");
+  const testFormRef = document.querySelector(".test-form");
+  const formItemsRef = document.querySelectorAll(".test-form fieldset");
+  const inputsRef = document.querySelectorAll("input[type=radio]");
+  const progressBarRef = document.querySelector(".progress-bar");
+  const loaderRef = document.querySelector(".result-load");
   let currentStep = 0;
 
   function updateProgress() {
-    const percent = (currentStep + 1) / formItems.length * 100;
-    progressBar.style.width = percent + "%";
+    const percent = (currentStep / (formItemsRef.length - 1)) * 100;
+    progressBarRef.style.width = percent + "%";
   }
 
   function showStep(stepIndex) {
-    formItems.forEach((step, index) => {
+    formItemsRef.forEach((step, index) => {
       if (index === stepIndex) {
         step.style.display = "flex";
       } else {
@@ -18,18 +21,34 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  function nextStep() {
-    if (currentStep < formItems.length - 1) {
+  function nextStep(e) {
+    if (currentStep < formItemsRef.length - 1) {
       currentStep++;
       showStep(currentStep);
       updateProgress();
     }
+    if (e.target.name === formItemsRef.length.toString()) {      
+      testFormRef.style.display = "none";
+      loaderRef.style.display = "block";
+    }
   }
-  const nextStepButton = document.querySelector("#next-btn");
-  nextStepButton.addEventListener("click", nextStep);
-  //   nextStepButtons.forEach((button) => {
-  //     button.addEventListener("click", nextStep);
-  //   });
+
+  function handleButton(e) {
+    const targetBtnRef = document.querySelector(
+      `.cta-btn.form[name="${e.target.name}"]`
+    );
+    targetBtnRef.removeAttribute("disabled");
+    targetBtnRef.classList.remove("disabled");
+    targetBtnRef.classList.add("active");
+  }
+  const nextStepButtons = document.querySelectorAll(".cta-btn.form");
+
+  nextStepButtons.forEach((button) => {
+    button.addEventListener("click", nextStep);
+  });
+  inputsRef.forEach((input) => {
+    input.addEventListener("click", handleButton);
+  });
   showStep(currentStep);
   updateProgress();
 });
